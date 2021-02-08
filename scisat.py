c765c1034c168c2c328fc1d2bab7bdcc580306cc
dreneuw <davrene11@gmail.com>
2021-02-08 10:06:51 -0500

    Fixed KeyLanguage error

--- scisat.py
+++ scisat.py
@@ -1758,7 +1758,8 @@ def get_locale():
         language = None
     if language is not None:
         return language
-    return 'en'
+    else:
+        return 'en'
 
 
 @app.server.route('/language/<language>')

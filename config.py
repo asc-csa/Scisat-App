e69cd741ab517abd1a9a147407671d8fc4e1ab62
nfee006 <natasha.l.fee@gmail.com>
2021-05-25 12:27:00 -0700

    Apache config - /app/ routing
    Change route to /app/ for integration with prod apache server
    Logo link now relative

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os.path
 class Config:
     LANGUAGES = {'en': 'English', 'fr': 'French'}
     DEFAULT_LANGUAGE = 'en'
-    APP_PREFIX = '/scisat/'
+    APP_PREFIX = '/app/scisat/'
     DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + r'/../../data'
-    EN_LINK = '/scisat' #url for the english version
-    FR_LINK = '/scisat-fr' #url for the french version
+    EN_LINK = '../app/scisat' #url for the english version
+    FR_LINK = '../app/scisat-fr' #url for the french version

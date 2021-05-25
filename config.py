01a19ec486b6e9839332d8b63c1fc7e59246ebb6
nfee006 <natasha.l.fee@gmail.com>
2021-05-25 12:37:29 -0700

    Fix for EN_LINK and FR_LINK

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
     DEFAULT_LANGUAGE = 'en'
     APP_PREFIX = '/app/scisat/'
     DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + r'/../../data'
-    EN_LINK = '../app/scisat' #url for the english version
-    FR_LINK = '../app/scisat-fr' #url for the french version
+    EN_LINK = '/app/scisat' #url for the english version
+    FR_LINK = '/app/scisat-fr' #url for the french version
